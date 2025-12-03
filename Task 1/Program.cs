bca09c51cb89dd17a04969264b56334ee183ff3c
Deepika Bhujel <dipikab062@gmail.com>
2025-12-03 11:22:58 +0545

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle { Length = 5, Width = 4 };
+            Console.WriteLine($"Area: {r.Area}");
+            Console.WriteLine($"Perimeter: {r.GetPerimeter()}");
+        }
+    }
+}
---
+++ Task 1/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Rectangle
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public double Area => Length * Width;
+
+        public double GetArea() => Length * Width;
+
+        public double GetPerimeter() => 2 * (Length + Width);
+    }
+}
